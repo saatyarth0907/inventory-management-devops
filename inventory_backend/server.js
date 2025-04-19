03f68f637c55a8f02158ad5a312a3b94abb86de9
saatyarth0907 <113109063+saatyarth0907@users.noreply.github.com>
2025-04-19 14:03:29 +0530

    Update server.js

--- inventory_backend/server.js
+++ inventory_backend/server.js
@@ -15,7 +15,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+app.listen(5000, '0.0.0.0', () => console.log("Server running on http://0.0.0.0:5000"));
 
 async function handleLogin(event) {
   event.preventDefault();
